4e8ad6565218d358fcea03e828db3bd266b177b9
taarsinii <taarsiniiaarsh@gmail.com>
2025-04-19 03:23:15 +0800

    Animation to the frontpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // main.dart
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'dart:math'; // For the circle animation
+
 import 'signIn_page.dart';
 import 'signup_page.dart';
 
@@ -35,71 +37,158 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Offset> _offsetAnimation;
+    with TickerProviderStateMixin {
+  late AnimationController _zoomController;
+  late Animation<double> _zoomAnimation;
+  late AnimationController _circleController;
+  late Animation<double> _circleAnimation;
+  bool _animationStarted = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 500), // Adjust duration as needed
+    _zoomController = AnimationController(
+      duration: const Duration(milliseconds: 500),
       vsync: this,
     );
+    _zoomAnimation = Tween<double>(begin: 1.0, end: 5.0).animate(
+      CurvedAnimation(parent: _zoomController, curve: Curves.easeInOut),
+    );
 
-    _offsetAnimation = Tween<Offset>(
-      begin: Offset.zero,
-      end: const Offset(0.0, -1.0), // Swipe the whole page up
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    ));
-  }
+    _circleController = AnimationController(
+      duration: const Duration(milliseconds: 700),
+      vsync: this,
+    );
+    _circleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _circleController, curve: Curves.easeInOut),
+    );
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _handleTap() {
-    _controller.forward().whenComplete(() {
-      Navigator.pushReplacementNamed(context, '/signin');
+    _zoomController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _circleController.forward().whenComplete(() {
+          Navigator.pushReplacementNamed(context, '/signin');
+        });
+      }
     });
   }
 
   @override
+  void dispose() {
+    _zoomController.dispose();
+    _circleController.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    if (!_animationStarted) {
+      setState(() {
+        _animationStarted = true;
+      });
+      _zoomController.forward();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // The Splash Screen Content
-        GestureDetector(
-          onTap: _handleTap,
-          child: Scaffold(
-            backgroundColor:
-                Colors.white, // Or any color you prefer for the splash screen
-            body: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Image.asset(
-                    'assets/images/the_shipcampus_logo.jpg',
-                    height: 500, // Increased the height of the logo
-                  ),
-                ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background Gradient
+          Positioned.fill(
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.red, Colors.blue],
+                ),
               ),
             ),
           ),
-        ),
-        // The Sign In Page (initially positioned below)
-        SlideTransition(
-          position: _offsetAnimation,
-          child: FractionalTranslation(
-            translation: const Offset(0.0, 1.0), // Start below the screen
-            child: const SignInPage(),
+          // Logo Zoom
+          Center(
+            child: GestureDetector(
+              onTap: _handleTap,
+              child: AnimatedBuilder(
+                animation: _zoomAnimation,
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: _zoomAnimation.value,
+                    child: Opacity(
+                      opacity: _animationStarted
+                          ? 1.0 - _circleAnimation.value
+                          : 1.0,
+                      child: Container(
+                        width: 400, // Adjust initial logo size
+                        height: 400,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white.withAlpha(
+                              (0.8 * 255).toInt()), // Corrected opacity
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ClipOval(
+                            child: Image.asset(
+                              'assets/images/the_shipcampus_logo.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-        ),
-      ],
+          // Circular Reveal
+          AnimatedBuilder(
+            animation: _circleAnimation,
+            builder: (context, child) {
+              double radius = MediaQuery.of(context).size.width *
+                  _circleAnimation.value *
+                  1.2; // Expand to cover the screen
+              return Center(
+                child: ClipPath(
+                  clipper: CircleClipper(radius),
+                  child: Container(
+                    color: Colors
+                        .white, // The color of your sign-in page background
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Opacity(
+                      opacity: _circleAnimation.value,
+                      child: const SignInPage(), // Your sign-in page
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class CircleClipper extends CustomClipper<Path> {
+  final double radius;
+  CircleClipper(this.radius);
+
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.addOval(Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: radius,
+    ));
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true; // Reclip on every animation frame
   }
 }
--- lib/signIn_page.dart
+++ lib/signIn_page.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'signUp_page.dart';
-import 'events_page.dart'; 
+import 'events_page.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -24,9 +24,12 @@ class _SignInPageState extends State<SignInPage> {
     if (!_formKey.currentState!.validate()) return;
 
     // Check for the required email domain
-    if (!emailController.text.toLowerCase().endsWith("@peninsulamalaysia.edu.my")) {
+    if (!emailController.text
+        .toLowerCase()
+        .endsWith("@peninsulamalaysia.edu.my")) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Please use your @peninsulamalaysia.edu.my email.")),
+        SnackBar(
+            content: Text("Please use your @peninsulamalaysia.edu.my email.")),
       );
       return;
     }
@@ -41,12 +44,13 @@ class _SignInPageState extends State<SignInPage> {
         email: emailController.text.trim(),
         password: passwordController.text.trim(),
       );
-      
+
       // Retrieve user role from Firestore
       String uid = userCredential.user!.uid;
-      DocumentSnapshot userDoc = await _firestore.collection("users").doc(uid).get();
-      
-       if (!userDoc.exists) {
+      DocumentSnapshot userDoc =
+          await _firestore.collection("users").doc(uid).get();
+
+      if (!userDoc.exists) {
         throw Exception("User record not found");
       }
       String role = userDoc.get('role') as String;
@@ -57,7 +61,7 @@ class _SignInPageState extends State<SignInPage> {
       // } else {
       //   throw Exception("User data not found");
       // }
-      
+
       // Navigate to EventsPage, passing the isAdmin flag
       Navigator.pushReplacement(
         context,
@@ -88,7 +92,11 @@ class _SignInPageState extends State<SignInPage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(title, style: TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.w500)),
+          Text(title,
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500)),
           SizedBox(height: 10),
           TextFormField(
             controller: controller,
@@ -129,7 +137,11 @@ class _SignInPageState extends State<SignInPage> {
               color: Colors.blue[100],
               padding: EdgeInsets.symmetric(vertical: 20),
               child: Center(
-                child: Text('SIGN IN', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black)),
+                child: Text('SIGN IN',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
               ),
             ),
             Expanded(
@@ -137,9 +149,12 @@ class _SignInPageState extends State<SignInPage> {
                 child: Column(
                   children: [
                     SizedBox(height: 20),
-                    Image.asset('assets/images/ship_logo.jpg', width: 200, height: 200),
+                    Image.asset('assets/images/ship_logo.jpg',
+                        width: 200, height: 200),
                     SizedBox(height: 5),
-                    Text('Student Engagement App', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
+                    Text('Student Engagement App',
+                        style: TextStyle(
+                            fontSize: 26, fontWeight: FontWeight.bold)),
                     SizedBox(height: 40),
                     _buildInputField(
                       title: 'Student Email Address',
@@ -164,11 +179,16 @@ class _SignInPageState extends State<SignInPage> {
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blue,
                             padding: EdgeInsets.symmetric(vertical: 15),
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5)),
                           ),
                           child: isLoading
                               ? CircularProgressIndicator(color: Colors.white)
-                              : Text('SIGN IN', style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+                              : Text('SIGN IN',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
                         ),
                       ),
                     ),
@@ -178,15 +198,21 @@ class _SignInPageState extends State<SignInPage> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Don't have an account?", style: TextStyle(fontSize: 18)),
+                          Text("Don't have an account?",
+                              style: TextStyle(fontSize: 18)),
                           TextButton(
                             onPressed: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => SignUpPage()),
+                                MaterialPageRoute(
+                                    builder: (context) => SignUpPage()),
                               );
                             },
-                            child: Text('Sign Up', style: TextStyle(fontSize: 18, decoration: TextDecoration.underline, color: Colors.blue)),
+                            child: Text('Sign Up',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    decoration: TextDecoration.underline,
+                                    color: Colors.blue)),
                           ),
                         ],
                       ),
